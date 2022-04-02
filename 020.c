8b135b9a301b27f8f8f2bb92640ab105844dad67
158bubbles <49231263+158bubbles@users.noreply.github.com>
2022-04-02 02:07:10 +0100

    Update 020.c

--- 020.c
+++ 020.c
@@ -1,1 +1,12 @@
+int contaPal (char s[]) {
+    int i, res = 0;
+    
+    for(i=0; s[i] != '\0'; i++){
+        if((!(isspace(s[i])) && isspace(s[i+1])) || (!(isspace(s[i])) && s[i+1] == '\0')){
+            res++;
+            
+        }
+    }
 
+    return res;
+}
