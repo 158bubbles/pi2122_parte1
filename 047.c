dd14506bd0a8a1791e59cfc9098a880869bf2670
158bubbles <49231263+158bubbles@users.noreply.github.com>
2022-04-03 18:01:58 +0100

    Create 047.c

---
+++ 047.c
@@ -0,0 +1,12 @@
+Posicao posFinal (Posicao inicial, Movimento mov[], int N){
+    int i;
+    
+    for(i=0; i < N; i++){
+        
+        if(mov[i] == Norte) inicial.y++;
+        else if(mov[i] == Sul) inicial.y--;
+        else if(mov[i] == Este) inicial.x++;
+        else if(mov[i] == Oeste) inicial.x--;
+    }
+    return inicial;
+}
